70ef0320ec8754293654526e5ee6bbe8290c713a
Ahmedou khlil <ouldkhlil09@gmail.com>
2026-05-15 13:21:18 +0000

    fix: page /users utilise les classes modal-overlay/modal-box du projet

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="fixed inset-0 bg-black/50 z-40 flex items-center justify-center p-4">
-    <div class="bg-white rounded-2xl shadow-2xl w-full max-w-5xl flex flex-col" style="max-height: calc(100vh - 2rem);">
+<div class="modal-overlay">
+    <div class="modal-box max-w-5xl w-full">
         <div class="modal-header">
             <h2><i class="fas fa-users-cog mr-2"></i>Gestion des utilisateurs</h2>
             <a href="{{ route('accueil.patient') }}" class="modal-close"><i class="fas fa-times"></i></a>
